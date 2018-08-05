f91a7e6d3697f51fe765b862e8f87405554a0088
alyssa0528 <alyssa.kim@gmail.com>
2018-08-05 07:42:48 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post "/users/new", to: "users#create"
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:new, :show, :create]
   resources :attractions, only: [:index, :show]
 end
